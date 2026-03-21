59cc01505658351ce4a182181b4834546144ae03
jihanv <61974348+jihanv@users.noreply.github.com>
2026-03-21 11:06:39 +0900

    refactor: move retriever to a separate utils file.

--- chat.ts
+++ chat.ts
@@ -1,35 +1,12 @@
 import "dotenv/config";
-import { createClient } from "@supabase/supabase-js";
-import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
-import { OpenAIEmbeddings } from "@langchain/openai";
 import { ChatOpenAI } from "@langchain/openai";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-
+import { retriever } from "./lib/utils";
 // document.addEventListener("submit", (e: SubmitEvent) => {
 //   e.preventDefault();
 //   progressConversation();
 // });
-
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-
-const embeddings = new OpenAIEmbeddings({ openAIApiKey: OPENAI_API_KEY });
-
-const SUPABASE_PUBLIC_KEY = process.env.SUPABASE_PUBLIC_KEY;
-const SUPABASE_URL = process.env.SUPABASE_URL;
-
-if (!SUPABASE_PUBLIC_KEY || !SUPABASE_URL) {
-  throw new Error("Missing SUPABASE_URL or SUPABASE_PUBLIC_KEY");
-}
-const client = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
-
-const vectorStore = new SupabaseVectorStore(embeddings, {
-  client,
-  tableName: "documents",
-  queryName: "match_documents",
-});
-
-const retriever = vectorStore.asRetriever();
 
 const llm = new ChatOpenAI({
   model: "gpt-4.1",
@@ -43,13 +20,13 @@ const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
   standaloneQuestionTemplate,
 );
 
-const standaloneQuestionChain = standaloneQuestionPrompt
+const chain = standaloneQuestionPrompt
   .pipe(llm)
   .pipe(new StringOutputParser())
   .pipe(retriever);
 
 async function main() {
-  const response = await standaloneQuestionChain.invoke({
+  const response = await chain.invoke({
     question:
       "「Clause」という概念がよく分かりません。それは何ですか？「Sentence」とはどう違うのですか？",
   });
---
+++ lib/utils.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { createClient } from "@supabase/supabase-js";
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const embeddings = new OpenAIEmbeddings({ openAIApiKey: OPENAI_API_KEY });
+
+const SUPABASE_PUBLIC_KEY = process.env.SUPABASE_PUBLIC_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+
+if (!SUPABASE_PUBLIC_KEY || !SUPABASE_URL) {
+  throw new Error("Missing SUPABASE_URL or SUPABASE_PUBLIC_KEY");
+}
+const client = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
+
+const vectorStore = new SupabaseVectorStore(embeddings, {
+  client,
+  tableName: "documents",
+  queryName: "match_documents",
+});
+
+const retriever = vectorStore.asRetriever();
+
+export { retriever };
